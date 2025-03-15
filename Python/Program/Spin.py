5f2ec6eb49f7c27d01a4f19e35ab62d5c0dcf90d
Kasyful Haq B. <82114912+DatBoiSUS-Baka@users.noreply.github.com>
2025-03-16 03:15:58 +0700

    Menambah kelas Psin.py

---
+++ Python/Program/Spin.py
@@ -0,0 +1,11 @@
+import BasicMove
+
+class Spin(BasicMove):
+    def __init__(self, name, duration, difficulty, speed):
+        super().__init__(name, duration, difficulty)
+        self.__spinSpeed = speed
+    
+    def setSpinSpeed(self, speed):
+        self.__spinSpeed = speed
+    def getSpinSpeed(self):
+        return self.__spinSpeed
