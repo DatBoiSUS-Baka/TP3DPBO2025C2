333a8302b61803145b6fd57593c83407933d34e7
Kasyful Haq B. <82114912+DatBoiSUS-Baka@users.noreply.github.com>
2025-03-16 03:21:23 +0700

    Menambahkan kelas Step.py

---
+++ Python/Program/Step.py
@@ -0,0 +1,11 @@
+import BasicMove
+
+class Step(BasicMove):
+    def __init__(self, name, duration, difficulty, intensity):
+        super().__init__(name, duration, difficulty)
+        self.__stepIntensity = intensity
+    
+    def setStepIntensity(self, intensity):
+        self.__stepIntensity = intensity
+    def getStepIntensity(self):
+        return self.__stepIntensity
